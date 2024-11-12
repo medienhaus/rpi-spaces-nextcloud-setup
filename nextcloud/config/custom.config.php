de63be7ab32c1e163e46c55328877d5f1156030f
Andi Rueckel <andirueckel@users.noreply.github.com>
2024-11-12 11:30:13 +0100

    initial commit

---
+++ nextcloud/config/custom.config.php
@@ -0,0 +1,6 @@
+<?php
+// see: https://docs.nextcloud.com/server/30/admin_manual/configuration_server/config_sample_php_parameters.html#skeletondirectory
+//
+$CONFIG = array (
+    'skeletondirectory' => '',
+);
